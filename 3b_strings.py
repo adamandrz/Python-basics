38da3ce8c4d03657263702007065688a34b3764a
adamandrz <adaman@niepije.org>
2019-09-23 14:40:01 +0200

    first commit

---
+++ 3b_strings.py
@@ -0,0 +1,23 @@
+print("This is a line of test!")
+
+print("This is a line of test!\n This is a second line!")
+
+print("This is some \" text")
+
+phrase = "String in a variable"
+print(phrase)
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+
+print(phrase.upper())
+
+print(phrase.isupper())
+
+print(phrase.islower())
+
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[2])
