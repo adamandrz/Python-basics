b61032ef13580faa3b94e93ec9a223491ecbadf2
adamandrz <adaman@niepije.org>
2019-09-24 09:28:38 +0200

    Variables lab tests

---
+++ 3e_input.py
@@ -0,0 +1,4 @@
+variable = input("Whats is your name?: ")
+print("Hello " + variable + " !")
+#print("Hello " + int(variable) + " !")
+#input zawsze pobiera zmienne do str, nie mozna mieszac typow zmiennych
