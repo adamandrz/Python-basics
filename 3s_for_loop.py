ca6313447e045b6716ac3f6767b2cacfb9bbe446
adamandrz <adaman@niepije.org>
2019-09-24 14:24:59 +0200

    Loop functions

---
+++ 3s_for_loop.py
@@ -0,0 +1,20 @@
+for my_var in "juniper":
+    print(my_var)
+
+friends = ["jim", "carren", "kevin"]
+for my_var2 in friends:
+    print(my_var2)
+
+for my_var3 in range(10):
+    print(my_var3)
+
+for my_var4 in range(len(friends)):
+    print(my_var4)
+
+for my_var5 in range(len(friends)):
+    if my_var5 == 0:
+        print("First interation")
+    else:
+        print("Not first")
+
+
